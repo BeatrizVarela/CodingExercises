a6f025af4dd9972d41aa1fa9c460fbc117a75eb2
Beatriz Varela <varela.ana.beatriz@gmail.com>
2023-04-20 17:55:28 +0100

    starting the stopwatch

--- Alura Courses/React Typescript/alura-studies/src/components/Stopwatch/index.tsx
+++ Alura Courses/React Typescript/alura-studies/src/components/Stopwatch/index.tsx
@@ -1,14 +1,24 @@
-import { timeToSeconds } from '../../common/utils/date';
+import { useState } from 'react';
+import { timeToSeconds } from '../../common/utils/time';
+import { ITask } from '../../types/task';
 import Button from '../Button';
 import Clock from './Clock';
 import style from './Stopwatch.module.scss'
 
-export default function Stopwatch() {
-  console.log('conversion: ', timeToSeconds("01:01:01"));
+interface Props {
+  selected: ITask | undefined
+}
+
+export default function Stopwatch({ selected }: Props) {
+  const [time, setTime] = useState<number>();
+  if(selected?.time) {
+    setTime(timeToSeconds(selected?.time));
+  }
   return (
     <>
       <div className={style.stopwatch}>
         <p className={style.title}>Chose a card and start the stopwatch:</p>
+        Time: {time}
         <div className={style.clockWrapper}>
           <Clock />
         </div>
