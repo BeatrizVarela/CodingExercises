75ca7925c20a132e1f95fec4a58ddab42b9804b9
Beatriz Varela <varela.ana.beatriz@gmail.com>
2023-04-21 17:33:38 +0100

    configuring stopwatch's minutes and seconds

--- Alura Courses/React Typescript/alura-studies/src/components/Stopwatch/index.tsx
+++ Alura Courses/React Typescript/alura-studies/src/components/Stopwatch/index.tsx
@@ -1,38 +1,35 @@
-import { useEffect, useState } from 'react';
-import React from 'react';
+import { useEffect, useState } from "react";
+import React from "react";
 
-import { timeToSeconds } from '../../common/utils/time';
-import { ITask } from '../../types/task';
-import Button from '../Button';
+import { timeToSeconds } from "../../common/utils/time";
+import { ITask } from "../../types/task";
+import Button from "../Button";
 
-import Clock from './Clock';
-import style from './Stopwatch.module.scss';
+import Clock from "./Clock";
+import style from "./Stopwatch.module.scss";
 
 interface Props {
-  selected: ITask | undefined
+  selected: ITask | undefined;
 }
 
 export default function Stopwatch({ selected }: Props) {
   const [time, setTime] = useState<number>();
 
   useEffect(() => {
-    if(selected?.time) {
+    if (selected?.time) {
       setTime(timeToSeconds(selected.time));
     }
-  },[selected])
-  
+  }, [selected]);
+
   return (
     <>
       <div className={style.stopwatch}>
         <p className={style.title}>Chose a card and start the stopwatch:</p>
-        Time: {time}
         <div className={style.clockWrapper}>
-          <Clock />
+          <Clock time={time} />
         </div>
-        <Button>
-          Start!
-        </Button>
+        <Button>Start!</Button>
       </div>
     </>
-  )
+  );
 }
