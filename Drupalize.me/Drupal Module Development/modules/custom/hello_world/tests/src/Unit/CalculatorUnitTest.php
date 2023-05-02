31425436bff635b9d02b8833faa5604dba1dbb48
Beatriz Varela <varela.ana.beatriz@gmail.com>
2023-05-02 16:48:37 +0100

    module hello_world

---
+++ Drupalize.me/Drupal Module Development/modules/custom/hello_world/tests/src/Unit/CalculatorUnitTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\Tests\hello_world\Unit;
+
+use Drupal\hello_world\Calculator;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests the Calculator class methods.
+ *
+ * @group hello_world
+ */
+class CalculatorUnitTest extends UnitTestCase {
+
+  /**
+   * Tests the Calculator::add() method.
+   */
+  public function testAdd() {
+    $calculator = new Calculator(10, 5);
+    $this->assertEquals(15,
+    $calculator->add());
+  }
+
+  /**
+   * Tests the Calculator::subtract() method.
+   */
+  public function testSubtract() {
+    $calculator = new Calculator(10, 5);
+    $this->assertEquals(5,
+    $calculator->subtract());
+  }
+
+  /**
+   * Tests the Calculator::multiply() method.
+   */
+  public function testMultiply() {
+    $calculator = new Calculator(10, 5);
+    $this->assertEquals(50,
+    $calculator->multiply());
+  }
+
+  /**
+   * Tests the Calculator::divide() method.
+   */
+  public function testDivide() {
+    $calculator = new Calculator(10, 5);
+    $this->assertEquals(2,
+    $calculator->divide());
+  }
+
+}
